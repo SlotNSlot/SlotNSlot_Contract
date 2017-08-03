3a23c83a640edd79d9db4afbf4b4f1a083ed0445
hoyeehoyee <mylovechristina@hotmail.com>
2017-08-03 16:56:50 +0900

    Added test script

--- test/test.js
+++ test/test.js
@@ -4,121 +4,137 @@ const Dispatcher = artifacts.require('Dispatcher.sol');
 const DispatcherStorage = artifacts.require('DispatcherStorage.sol');
 const SlotLib = artifacts.require("./sns/SlotLib.sol");
 const SlotMachineManager = artifacts.require("./sns/SlotMachineManager.sol");
-const SlotLib2 = artifacts.require("./sns/SlotLib2.sol");
 const SlotMachineStorage = artifacts.require("./sns/SlotMachineStorage.sol");
 const PaytableStorage = artifacts.require("./sns/PaytableStorage.sol");
+const SlotMachine = artifacts.require("./sns/SlotMachine.sol");
+
+function makeseed(origin, iternum) {
+  var seed = origin.toString(16);
+  for (var i = 0; i < iternum; i++) {
+    seed = web3.sha3(seed,{encoding:'hex'});
+  }
+  return seed;
+}
+
 
 contract('TestProxyLibrary', () => {
   describe('test', () => {
+
     it('works', () => {
-      var slotManager, slotStorage;
+
+      var slotManager, slotStorage, slot, slotaddr;
       var dispatcherStorage;
-      var creationevent, removalevent, numberevent;
+      var bankerBalance, playerBalance;
+      var user1 = web3.eth.accounts[0];
+      var user2 = web3.eth.accounts[1];
+      var playerNumbers = [1,1,1].map(function(x){return Math.random()});
+      var bankerNumbers = [1,1,1].map(function(x){return Math.random()});
+      var playerSeeds = playerNumbers.map(function(x){return makeseed(x,10)});
+      var bankerSeeds = bankerNumbers.map(function(x){return makeseed(x,10)});
+      function printBalance() {
+        return (slot.playerBalance()).then(playerbalance=>{
+          console.log('playerBalance : ', playerbalance.valueOf().toString());
+          return (slot.bankerBalance()).then(bankerbalance=>{console.log('bankerBalance : ', bankerbalance.valueOf().toString())})
+        });
+      }
+
+      function playGame(bet, line, idx, chainnum) {
+        console.log('------GAME START!------');
+        slot.initGameForPlayer(bet,line,idx,{from:user2});
+        return slot.initGameForPlayer.estimateGas(bet,line,idx,{from:user2}).then(result=>{
+          console.log('Game initialized, gasUsed : ', result);
+          slot.setBankerSeed(makeseed(bankerNumbers[idx],chainnum),idx);
+          return slot.setBankerSeed.estimateGas(makeseed(bankerNumbers[idx],chainnum),idx);
+        }).then(result => {
+          console.log('Banker seed is set, gasUsed : ', result);
+          slot.setPlayerSeed(makeseed(playerNumbers[idx],chainnum),idx,{from:user2});
+          return slot.setPlayerSeed.estimateGas(makeseed(playerNumbers[idx],chainnum),idx,{from:user2});
+        }).then(result => {
+          console.log('Player seed is set, gasUsed : ', result);
+          return slot.mGame(idx);
+        }).then(result => {
+          console.log('player initial betting : ', bet * line, 'player reward : ',result[5].valueOf().toString());
+          console.log('player final prize : ', result[5] - bet * line);
+          // console.log('player reward : ',result[5].valueOf().toString());
+          return printBalance();
+        })
+      }
 
       SlotMachineManager.deployed().then(function(instance) {
         slotManager = instance;
-        SlotMachineStorage.deployed().then(function(instance) {
+        return SlotMachineStorage.deployed().then(function(instance) {
           slotStorage = instance;
           slotStorage.transferOwnership(slotManager.address);
           console.log('SlotMachineStorage Owner changed to ', slotManager.address);
-          return PaytableStorage.deployed().then(function(instance) {
-            slotStorage.setPaytableStorage(instance.address);
-            console.log('PaytableStorage address : ', instance.address);
-          });
+          return;
         });
       })
       .then(() => {
-        console.log('setting up event watcher...');
-        creationevent = slotManager.slotMachineCreated();
-        creationevent.watch(function(error, result){
-          if (!error){
-            console.log('Event Log : \n \tslotMachine created in storage, provider : ', result.args._provider,
-              'decider : ', result.args._decider,
-              'minBet : ', result.args._minBet,
-              'maxBet : ', result.args._maxBet,
-              'maxPrize : ', result.args._maxPrize,
-              'slot totalnum : ', result.args._totalnum,
-              'slot address : ', result.args._slotaddr);
-          }
-        })
-      })
-      .then(() => {
-        removalevent = slotManager.slotMachineRemoved();
-        removalevent.watch(function(error, result){
-          if(!error){
-            console.log('Event Log : \n \tslotMachine removed in storage, provider : ', result.args._provider,
-            'removed slot addr : ', result.args._slotaddr,
-            'totalnum : ', result.args._totalnum);
-          }
-        });
-      })
-      .then(() => {
-        console.log('event watcher setting completed');
-        return  slotManager.getNumofSlotMachine();
+        console.log('Initializing test start')
+        return slotStorage.totalNumOfSlotMachine();
       })
       .then(result => {
-        console.log('initializing test, # of slotmachine should be 0');
-        assert.equal(result,0,'initializing test failed');
-        console.log('initializing test completed successfully');
-        slotManager.createSlotMachine(100, 1000, 10000, 1000);
-        slotManager.createSlotMachine(100, 2000, 20000, 1500);
-        slotManager.createSlotMachine(125, 3000, 30000, 1500);
-        slotManager.createSlotMachine(150, 4000, 40000, 2000);
-        slotManager.createSlotMachine(150, 5000, 50000, 2000);
-        return slotManager.getNumofSlotMachine();
+        assert.equal(result, 0, 'There are already some slotmachines in storage');
+        console.log('Initializing test completed successfully');
+        console.log('Creating test start');
+        slotManager.createSlotMachine(100, 1000, 10000, 1000, 'test1');
+        return slotStorage.totalNumOfSlotMachine();
       })
       .then(result => {
-        console.log('creating test, # of slotmachine should be 5');
-        assert.equal(result, 5, 'creating test failed');
-        console.log('creating test completed successfully');
+        assert.equal(result, 1, 'creating test1 failed');
+        console.log('Slotmachine1 is created successfully');
+        slotManager.createSlotMachine(150, 2000, 20000, 2000, 'test2');
+        return slotManager.createSlotMachine.estimateGas(150, 2000, 20000, 2000, 'test2');
+        // return slotStorage.totalNumOfSlotMachine();
+      })
+      .then(result => {
+        console.log('Creating test completed successfully, gasUsed : ', result);
+        return slotStorage.totalNumOfSlotMachine();
+      }).then(result => {
+        console.log('Total num of slotmachine : ', result.valueOf().toString());
+        return slotStorage.getSlotMachine(user1,1);
+      }).then(result => {
+        slotaddr = result;
+        console.log('slot address : ',slotaddr);
+        slot = SlotMachine.at(slotaddr);
+        web3.eth.sendTransaction({from:user1,to:slotaddr,value:300000});
+        return slot.getInfo();
+      }).then(slotinfo => {
+        console.log(slotinfo);
+        slot.occupy(playerSeeds,{from:user2,value:200000});
+        console.log('slot occupied by player : ', user2);
+        slot.initBankerSeed(bankerSeeds);
+        return printBalance();
+      }).then(result => {
+        console.log('Bankerseeds are initialized');
+        return playGame(5000,20,0,9);
+      }).then(() => {
+        return playGame(4000,20,0,8);
+      }).then(() => {
+        return playGame(4000,20,0,7);
+      }).then(() => {
+        return playGame(4000,20,0,6);
+      }).then(() => {
+        return playGame(4000,20,0,5);    
+      })
+      //   slot.initGameForPlayer(5000,20,0,{from:user2});
+      //   return slot.initGameForPlayer.estimateGas(5000,20,0,{from:user2});
+      // }).then(result => {
+      //   console.log('Game initialized, gasUsed : ', result);
+      //   slot.setBankerSeed(makeseed(bankerNumbers[0],9),0);
+      //   return slot.setBankerSeed.estimateGas(makeseed(bankerNumbers[0],9),0);
+      // }).then(result => {
+      //   console.log('Banker seed is set, gasUsed : ', result);
+      //   slot.setPlayerSeed(makeseed(playerNumbers[0],9),0,{from:user2});
+      //   return slot.setPlayerSeed.estimateGas(makeseed(playerNumbers[0],9),0,{from:user2});
+      // }).then(result => {
+      //   console.log('Player seed is set, gasUsed : ', result);
+      //   return slot.mGame(0);
+      // }).then(result => {
+      //   console.log('player reward : ',result[5].valueOf().toString());
+      //   return printBalance();
+      // })
 
-        console.log('removing test, # of slotmachine should be 4');
-        slotManager.removeSlotMachine(0);
-        return slotManager.getNumofSlotMachine();
-      })
-      .then(result => {
-        assert.equal(result, 4, 'removing test failed');
-        console.log('removing test completed successfully');
-        
-      })
-
-      .then(() => {
-        return DispatcherStorage.deployed().then(function (instance) {
-          dispatcherStorage = instance;
-          return dispatcherStorage.getLib();
-        })
-        .then(libaddr => {
-          console.log('current lib address : ', libaddr);
-        });
-      })
-      .then(() => {
-        return SlotLib2.deployed().then(function (instance) {
-          dispatcherStorage.replace(instance.address);
-
-          console.log('hihi', instance.address);
-          return dispatcherStorage.getLib();
-        })
-        .then(newlibaddr => {
-          console.log('changed lib address : ', newlibaddr);
-        });
-      })
-      .then(() => {
-        console.log('new library linking test start');
-        return slotManager.getSlotMachineDecider(0);
-      })
-      .then(result => {
-        console.log('new result is ', result);
-        assert.equal(result, 20, 'Decider should be 20, new library linking failed');
-        console.log('new library linking test completed successfully');
-       return slotManager.getSlotMachineDecider(0);
-      })
-      .then(result => {
-        console.log('first slot of user : ', result);
-      })
-      .then(() => {
-        console.log('test completed');
-      });
-
-      });
+    });
   });
 });
